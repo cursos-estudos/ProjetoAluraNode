76943795b79007d25f0c37c42087f7ff7e8c7840
Makinay <vitor_souza97@hotmail.com>
2021-08-25 03:44:25 -0300

    estou com sono

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const CustomExpress = require('./config/CustomExpress')
 const conexão = require('./database/conexão')
+const tabelas = require('./database/tabelas')
 
 conexão.connect(erro => {
     if(erro){
         console.log(erro)
     } else{
         console.log('conectado com sucesso')
+
+        tabelas.init(conexão)
 
         const app = CustomExpress()
 
